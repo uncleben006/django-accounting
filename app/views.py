4a712ff3784bfd2385094c847fe4ae876cf94a9d
ben_wang <uncleben006@gmail.com>
2020-06-03 15:01:19 +0800

    Add Bootstrap template

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def hello(request):
     return render(request,'app/hello.html',{})
+
+def index(request):
+    return render(request,'app/index.html',{})
