e007041d802bd241dcf90daecf8c9000ec3fe3d4
ben_wang <uncleben006@gmail.com>
2020-06-04 13:32:50 +0800

    Register models into admin

--- app/models.py
+++ app/models.py
@@ -6,9 +6,16 @@ BALANCE_TYPE = ((u'收入',u'收入'),(u'支出',u'支出'))
 class Category(models.Model):
     category = models.CharField(max_length=20)
 
+    # 當此物件要被 print 的時候，會以什麼字串作為顯示
+    def __str__(self):
+        return self.category
+
 class Record(models.Model):
     date = models.DateField()
     description = models.CharField(max_length=300)
     category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
     cash = models.IntegerField()
     balance_type = models.CharField(max_length=2,choices=BALANCE_TYPE)
+
+    def __str__(self):
+        return self.description
