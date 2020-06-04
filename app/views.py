e15f839c3e7042caf3432405df4bae2fed7fc142
ben_wang <uncleben006@gmail.com>
2020-06-04 14:54:52 +0800

    Pass value to template from view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, HttpResponse
+from .models import Record
 
 # Create your views here.
 def hello(request):
     return render(request,'app/hello.html',{})
 
 def index(request):
-    return render(request,'app/index.html',{})
+    records = Record.objects.filter()
+    income_list = [record.cash for record in records if record.balance_type == '收入']
+    outcome_list = [record.cash for record in records if record.balance_type == '支出']
+    income = sum(income_list) if len(income_list) != 0 else 0
+    outcome = sum(outcome_list) if len(outcome_list) != 0 else 0
+    net = income - outcome
+
+    # locals() 會直接 load 進這個 method 裡宣告的所有變數
+    return render(request,'app/index.html',locals())
