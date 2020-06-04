71eec4d68a0e2e73237787a2b165585429357966
ben_wang <uncleben006@gmail.com>
2020-06-04 13:13:29 +0800

    Init Model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+BALANCE_TYPE = ((u'收入',u'收入'),(u'支出',u'支出'))
+
+class Category(models.Model):
+    category = models.CharField(max_length=20)
+
+class Record(models.Model):
+    date = models.DateField()
+    description = models.CharField(max_length=300)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    cash = models.IntegerField()
+    balance_type = models.CharField(max_length=2,choices=BALANCE_TYPE)
